8b42a1e43273b018a30e26134eb44afa81b5dca2
yuanlin <adoulin1314@icloud.com>
2017-06-07 00:15:50 +0800

    start activity by intent

--- app/src/main/java/com/example/yuanlin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yuanlin/myapplication/MainActivity.java
@@ -1,21 +1,54 @@
 package com.example.yuanlin.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_DATA = "extra_data";
+    public static final String TAG = MainActivity.class.getSimpleName();
 
-    @Override
+    View.OnClickListener mListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            sendMessage();
+        }
+    };
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    // Example of a call to a native method
-/*    TextView tv = (TextView) findViewById(R.id.sample_text);
-    tv.setText(stringFromJNI());*/
+        // Example of a call to a native method
+        TextView tv = (TextView) findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+
+        Button send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(mListener);
+        Log.d(TAG, "setOnClickListener");
+/*        Button send = (Button) findViewById(R.id.send);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+
+        });*/
     }
 
+    private void sendMessage() {
+        Log.d(TAG, "sendMessage");
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        String msg = editText.getText().toString();
+        intent.putExtra(EXTRA_DATA, msg);
+        startActivity(intent);
+    }
     /**
      * A native method that is implemented by the 'native-lib' native library,
      * which is packaged with this application.
