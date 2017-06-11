bbd5cca5a5c054beb8f8c45e61c3666e3ed987a5
yuanlin <adoulin1314@icloud.com>
2017-06-11 16:25:03 +0800

    options menu

--- app/src/main/java/com/example/yuanlin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yuanlin/myapplication/MainActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_DATA = "extra_data";
@@ -29,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "you clicked add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "you clicked remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+
+        return true;
     }
 
     private void sendMessage() {
